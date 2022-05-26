d6f71c635b0a8b5b95886a8e6dd77191a34bfc83
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-27 00:15:40 +0300

    test

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,3 +1,4 @@
+from api.permissions import IsAdmin
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
@@ -7,8 +8,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
-from api.permissions import IsAdmin
 from api_yamdb.settings import SENDER_EMAIL
+
 from .models import User
 from .serializers import SignUpSerializer, TokenSerializer, UserSerializer
 
