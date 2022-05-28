327a0a0c1ad7fdfac3838eaffe1fe8d260800803
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-28 11:35:31 +0300

    text

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework import routers
-
 from users.views import MyUserViewSet, SignUpViewSet, TokenViewSet, UserViewSet
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
