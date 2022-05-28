004ed5f25f3d4c289a5e58cb9d97cfb0f4ba157c
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-28 19:46:54 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,8 +4,7 @@ from rest_framework import filters, mixins, viewsets
 from rest_framework.generics import get_object_or_404
 from reviews.models import Category, Genre, Review, Title
 
-from api.permissions import (IsAdminOrReadOnly,
-                             IsAuthorOrModeratorOrAdminOrReadOnly)
+from api.permissions import (IsAdminOrReadOnly, IsAuthorOrModeratorOrAdminOrReadOnly)
 from .filters import TitleFilter
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer,
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -8,7 +8,6 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
 from api_yamdb.settings import SENDER_EMAIL
-
 from api.permissions import IsAdmin
 from .models import User
 from .serializers import SignUpSerializer, TokenSerializer, UserSerializer
