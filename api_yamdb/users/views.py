f5079bf7a3a5679a451a3cf37572989699015a6c
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-28 20:11:50 +0300

    test

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,5 +1,4 @@
 from api.permissions import IsAdmin
-
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
