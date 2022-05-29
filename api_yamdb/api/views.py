57f2eabcdfbb066a760978bfef197df93305d9c9
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-29 16:22:46 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,7 +4,6 @@ from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
 from rest_framework.generics import get_object_or_404
-
 from reviews.models import Category, Genre, Review, Title
 
 from .filters import TitleFilter
