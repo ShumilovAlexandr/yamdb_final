8a1e1e5998579d5ff105c5b7d81b03577f84b4e2
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-05-29 15:20:57 +0300

    test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # yamdb_final
 yamdb_final
+
+
+![example workflow](https://github.com/ShumilovAlexandr/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg)
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -7,9 +7,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
+from api.permissions import IsAdmin
 from api_yamdb.settings import SENDER_EMAIL
 
-from api.permissions import IsAdmin
 from .models import User
 from .serializers import SignUpSerializer, TokenSerializer, UserSerializer
 
