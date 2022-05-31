d1b2890d9e002330bec9a7169ae8fae6f3104af5
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-06-01 00:12:27 +0300

    review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=546842153151)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.28.8']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
