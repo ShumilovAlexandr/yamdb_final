68c5345718fe0de7927c13272ad7120b509fa402
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-06-21 22:47:33 +0300

    t

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,13 +101,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = 'static'
 
-STSTICFILES_DIRS = BASE_DIR + 'staticfiles/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = BASE_DIR + 'media'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
