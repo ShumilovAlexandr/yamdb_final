f9c9f024ae1060324b89f847bbaae8ba478daadb
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-06-23 01:15:14 +0300

    master

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from django.conf import settings
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -101,8 +103,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
