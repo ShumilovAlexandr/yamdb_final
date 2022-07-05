361449c834813a5c4cc8f3e3772f8b7a91c2d8ce
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-07-05 18:48:58 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.getenv('SECRET_KEY', default=546842153151)
 
 DEBUG = False
-
 AUTH_USER_MODEL = 'users.User'
 
 ALLOWED_HOSTS = ['51.250.28.8', 'localhost', '127.0.0.1', 'web']
